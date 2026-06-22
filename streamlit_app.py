17c28c7562808e9a8a6665e3f1a2d3b05d07caac
Sean-Stilwell <sean.stilwell@ssc-spc.gc.ca>
2026-06-22 10:18:44 -0400

    catching errors on individual images

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,13 @@ import cv2
 
 AZURE_SAS_URI = os.environ.get("SAS_TOKEN")
 
-if not AZURE_SAS_URI:
-    st.error("Please set the SAS_TOKEN environment variable to run this app.")
+try:
+    base_uri, token = AZURE_SAS_URI.split("?", 1)
+    if not base_uri or not token:
+        raise ValueError
+except Exception as e:
+    st.error("Invalid SAS_TOKEN format. Please ensure it is in the format 'https://<storage_account>.blob.core.windows.net/<container>?<sas_token>'.")
     st.stop()
-
-base_uri, token = AZURE_SAS_URI.split("?", 1)
 
 st.set_page_config("Webb Space Telescope vs Hubble Telescope", "🔭")
 
@@ -20,36 +22,45 @@ st.write("")
 st.write("")
 
 st.markdown("### Southern Nebula")
-image_comparison(
-    img1=f"{base_uri}/hubble/southern_nebula_700.jpg?{token}",
-    img2=f"{base_uri}/webb/southern_nebula_700.jpg?{token}",
-    label1="Hubble",
-    label2="Webb",
-)
-
+try:
+    image_comparison(
+        img1=f"{base_uri}/hubble/southern_nebula_700.jpg?{token}",
+        img2=f"{base_uri}/webb/southern_nebula_700.jpg?{token}",
+        label1="Hubble",
+        label2="Webb",
+    )
+except Exception as e:
+    st.error("This image failed to load. Verify the file exists in the Azure Blob Storage")
 
 st.markdown("### Galaxy Cluster SMACS 0723")
-image_comparison(
-    img1=f"{base_uri}/hubble/deep_field_700.jpg?{token}",
-    img2=f"{base_uri}/webb/deep_field_700.jpg?{token}",
-    label1="Hubble",
-    label2="Webb",
-)
+try:
+    image_comparison(
+        img1=f"{base_uri}/hubble/deep_field_700.jpg?{token}",
+        img2=f"{base_uri}/webb/deep_field_700.jpg?{token}",
+        label1="Hubble",
+        label2="Webb",
+    )
+except Exception as e:
+    st.error("This image failed to load. Verify the file exists in the Azure Blob Storage")
 
 st.markdown("### Carina Nebula")
-image_comparison(
-    img1=f"{base_uri}/hubble/carina_2800.png?{token}",
-    img2=f"{base_uri}/webb/carina_2800.jpg?{token}",
-    label1="Hubble",
-    label2="Webb",
-)
+try:
+    image_comparison(
+        img1=f"{base_uri}/hubble/carina_2800.png?{token}",
+        img2=f"{base_uri}/webb/carina_2800.jpg?{token}",
+        label1="Hubble",
+        label2="Webb",
+    )
+except Exception as e:
+    st.error("This image failed to load. Verify the file exists in the Azure Blob Storage")
 
 st.markdown("### Stephan's Quintet")
-image_comparison(
-    img1=f"{base_uri}/hubble/stephans_quintet_2800.png?{token}",
-    img2=f"{base_uri}/webb/stephans_quintet_2800.jpg?{token}",
-    label1="Hubble",
-    label2="Webb",
-)
-
-
+try:
+    image_comparison(
+        img1=f"{base_uri}/hubble/stephans_quintet_2800.jpg?{token}",
+        img2=f"{base_uri}/webb/stephans_quintet_2800.jpg?{token}",
+        label1="Hubble",
+        label2="Webb",
+    )
+except Exception as e:
+    st.error("This image failed to load. Verify the file exists in the Azure Blob Storage")
